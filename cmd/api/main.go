c518f4db44a8bd00ac687f2c160650c380e30716
kabir-vats <kabir.vats.2004@gmail.com>
2025-04-09 14:18:18 -0700

    restructured

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+}
 
 func main() {
 	r := gin.Default()
