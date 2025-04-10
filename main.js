8867067b50a4938b16225c352e1e803d2af5767b
kabir-vats <kabir.vats.2004@gmail.com>
2025-04-10 00:45:19 -0700

    get summary stats and today events

--- main.js
+++ main.js
@@ -2,9 +2,29 @@
 import { google } from "googleapis";
 import { getSheetsService, getFirebaseService } from "./utils/service-auth.js";
 import { getEventsTable, getAttendanceTable } from "./utils/sheets.js";
+import {
+    countUniqueEmails,
+    countUniqueEvents,
+    countUniqueOrgs,
+    countTotalAllocated,
+    getTodaysEvents,
+} from "./utils/stats.js";
 
 const firebaseService = await getFirebaseService();
 const sheetsService = await getSheetsService();
 
+const eventsTable = await getEventsTable(sheetsService);
+const attendanceTable = await getAttendanceTable(sheetsService);
+
+console.log(countUniqueEmails(attendanceTable))
+console.log(countUniqueEvents(eventsTable))
+console.log(countUniqueOrgs(eventsTable))
+console.log(countTotalAllocated(eventsTable))
+console.log(getTodaysEvents(eventsTable))
+
+
 console.log((await getEventsTable(sheetsService)).length)
 console.log((await getAttendanceTable(sheetsService)).length)
+
+const test = '$200.00'
+console.log(parseFloat(test.replace(/[$,]/g, '')))
--- utils/stats.js
+++ utils/stats.js
@@ -3,12 +3,67 @@ import dotenv from "dotenv";
 import { google } from "googleapis";
 dotenv.config();
 
-async function getEventsTable(googleSheetsService) {
-    const masterSheetId = process.env.MASTER_SHEET_ID;
-    const res = await googleSheetsService.spreadsheets.values.get({
-        spreadsheetId: masterSheetId,
-        range: "All Events Fall!A:C",
-    })
-    console.log(res.data.values[0])
+function countUniqueEmails(attendanceData) {
+    const uniqueEmails = new Set();
+    attendanceData.forEach((row) => {
+        if (row[1]) {
+            uniqueEmails.add(row[1]);
+        }
+    });
+    return uniqueEmails.size;
 }
 
+/*function getAttendanceLeaderboard(eventData, attendanceData) {
+
+}*/
+
+function countUniqueEvents(eventData) {
+    const uniqueEvents = new Set();
+    eventData.forEach((row) => {
+        if (row[0]) {
+            uniqueEvents.add(row[0]);
+        }
+    });
+    return uniqueEvents.size;
+}
+
+function countUniqueOrgs(eventData) {
+    const uniqueOrgs = new Set();
+    eventData.forEach((row) => {
+        if (row[1]) {
+            uniqueOrgs.add(row[1]);
+        }
+    });
+    return uniqueOrgs.size;
+}
+
+function countTotalAllocated(eventData) {
+    let totalAllocated = 0;
+    eventData.forEach((row) => {
+        if (row[5]) {
+            const value = parseFloat(row[5].replace(/[$,]/g, ''));
+            if (!isNaN(value)) {
+                totalAllocated += value;
+            }
+        }
+    });
+    return totalAllocated;
+}
+
+function getTodaysEvents(eventData) {
+    const today = new Date();
+    const todaysEvents = eventData.filter((row) => {
+        const eventDate = new Date(row[3]);
+        return eventDate.toDateString() === today.toDateString();
+    });
+    return todaysEvents;
+}
+
+export {
+    countUniqueEmails,
+    countUniqueEvents,
+    countUniqueOrgs,
+    countTotalAllocated,
+    getTodaysEvents,
+    // getAttendanceLeaderboard,
+}
